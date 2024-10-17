6c36f1edd8d21d5c5675fb8b96bca185c135c9e8
rubaiiyat <abirr1516@gmail.com>
2024-10-17 23:59:01 +0600

    age validation

---
+++ 02-Control-Flow/02-check-age.js
@@ -0,0 +1,5 @@
+const name = "Abir";
+const age = 60;
+
+if (age >= 18 && age <= 60) console.log(name, "is eligible of driving licence");
+else console.log(name, "is not eligible of driving licence");
