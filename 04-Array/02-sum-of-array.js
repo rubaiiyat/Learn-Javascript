f3fe09a1896006918ef77872039e52741e92c528
rubaiiyat <abirr1516@gmail.com>
2024-10-18 14:03:21 +0600

    Find largest element of array

---
+++ 04-Array/02-sum-of-array.js
@@ -0,0 +1,9 @@
+let arr = [4, 2, 6, 1, 7, 2, 4];
+
+sum = 0;
+
+for (ar of arr) {
+  sum += ar;
+}
+
+console.log(`Sum of Array is: ${sum}`);
---
+++ 04-Array/03-Find-largest-element.js
@@ -0,0 +1,5 @@
+let arr = [5, 2, 9, 1, 5, 6];
+
+arr.sort();
+
+console.log(arr[arr.length - 1]);
