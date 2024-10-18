926395bf5018136d6191e49da1a5da8d7432d9a8
rubaiiyat <abirr1516@gmail.com>
2024-10-18 18:12:11 +0600

    Find the Index of an Element

---
+++ 04-Array/13-find-index-of-element.js
@@ -0,0 +1,9 @@
+let arr = [10, 20, 30, 40];
+let element = 30;
+
+for (let index = 0; index < arr.length; index++) {
+  if (arr[index] == element) {
+    console.log("Index is: ", index);
+    break;
+  }
+}
