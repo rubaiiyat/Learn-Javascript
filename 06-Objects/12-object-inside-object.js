5726bf9c33e40cf54b4d794495529bb784310d13
rubaiiyat <abirr1516@gmail.com>
2024-10-21 16:38:31 +0600

    Add object inside object

---
+++ 06-Objects/12-object-inside-object.js
@@ -0,0 +1,12 @@
+let person = {
+  name: "Abir",
+
+  subject: {
+    math: 80,
+    physics: 60,
+  },
+};
+
+console.log(person);
+console.log(person.subject.math);
+console.log(person.subject.physics);
