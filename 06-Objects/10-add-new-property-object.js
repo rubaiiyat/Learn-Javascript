8774d0673a91ca46f710e0e6fe878b0fb7c723a5
rubaiiyat <abirr1516@gmail.com>
2024-10-21 16:32:36 +0600

    Add new property at object

---
+++ 06-Objects/10-add-new-property-object.js
@@ -0,0 +1,13 @@
+let person = {
+  name: "Abir",
+  age: 22,
+  address: "Bogura",
+};
+
+person.country = "UK";
+
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.address);
+console.log(person.country);
