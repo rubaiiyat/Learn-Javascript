9e585169ed5567a5f71dbb282f0b19c0a8ce27d3
rubaiiyat <abirr1516@gmail.com>
2024-10-25 17:24:03 +0600

    working with objects and properties

---
+++ 09-OOP/objects-properties.js
@@ -0,0 +1,13 @@
+const person = {
+  firstName: "John",
+  lastName: "Lennon",
+  age: 70,
+  location: "USA",
+  from: "The Beatles",
+  doing: function () {
+    console.log("Doing something for human rights");
+  },
+};
+
+console.log(person.firstName);
+person.doing();
