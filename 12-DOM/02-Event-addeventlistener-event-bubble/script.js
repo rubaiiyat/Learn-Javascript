b5adcd35e763583c19710f135c01e85fb83b9320
rubaiiyat <abirr1516@gmail.com>
2024-11-02 22:30:18 +0600

    Add comment and increase desrease value using events

--- 12-DOM/02-Event-addeventlistener-event-bubble/script.js
+++ 12-DOM/02-Event-addeventlistener-event-bubble/script.js
@@ -5,6 +5,56 @@ function changeColor() {
 
   const color = [color1, color2, color3];
 
-  const body = document.getElementsByTagName("*")[0];
-  body.style.backgroundColor = `rgb(${color.join(",")})`;
+  const btn = document.querySelector(".randomColor");
+  btn.style.backgroundColor = `rgb(${color.join(",")})`;
 }
+let val = 1;
+document.getElementById("decreaseID").addEventListener("click", function () {
+  if (val > 1) {
+    val -= 1;
+    const decreaseValue = document.getElementById("ourValue");
+    decreaseValue.innerText = val;
+  } else {
+    const decreaseValue = document.getElementById("ourValue");
+    decreaseValue.innerText = "Decrease not possible less than 1";
+  }
+
+  const decreaseValue = document.getElementById("ourValue");
+});
+
+document.getElementById("increaseID").addEventListener("click", function () {
+  val += 1;
+  const decreaseValue = document.getElementById("ourValue");
+  decreaseValue.innerText = val;
+});
+
+document.getElementById("resetID").addEventListener("click", function () {
+  val = 1;
+  const decreaseValue = document.getElementById("ourValue");
+  decreaseValue.innerText = val;
+});
+
+document
+  .getElementById("addCommentBtn")
+  .addEventListener("click", function (event) {
+    const commentUser = document.getElementById("username");
+    const commentUserValue = commentUser.value;
+
+    const comment = document.getElementById("comment");
+    const commentValue = comment.value;
+
+    const addComment = document.getElementById("addComment");
+
+    const newComment = document.createElement("div");
+    newComment.innerHTML = `
+    <h1 class="font-bold text-xl text-white">${commentUserValue}</h1>
+    <p class="text-sm">${commentValue}</p>
+    <button class="text-error mb-10" >Delete Comment</button>
+    
+    `;
+
+    addComment.appendChild(newComment);
+
+    commentUser.value = "";
+    comment.value = "";
+  });
