6a5b075916acc8b795befe1116807059731f35a0
rubaiiyat <abirr1516@gmail.com>
2024-11-04 14:44:51 +0600

    Calculate Geometry using function

--- 12-DOM/03-Calculate-Geometry/script.js
+++ 12-DOM/03-Calculate-Geometry/script.js
@@ -1,4 +1,58 @@
+function CalculateArea(num1, num2) {
+  const number1 = parseFloat(document.getElementById(num1).value);
+  const number2 = parseFloat(document.getElementById(num2).value);
+
+  if (!isNaN(number1) && !isNaN(number2)) {
+    const area = number1 * number2;
+    return area;
+  } else {
+    return "Insert valid input";
+  }
+}
+// Triangle
 document.getElementById("triangleBtn").addEventListener("click", function () {
+  let base = "base";
+  let height = "height";
+
+  let area = CalculateArea(base, height);
+
+  let displayValue = document.getElementById("displayValueTriangle");
+  if (typeof area === "number") {
+    displayValue.innerHTML = `
+      <p >Area (A) = ${0.5 * area} cm<sup>2</sup></p>
+    `;
+  } else {
+    displayValue.innerHTML = `
+      <p id="displayError">Insert Valid Input</p>
+    `;
+
+    let displayError = document.getElementById("displayError");
+    displayError.style.color = "#f23737";
+  }
+});
+// Rectangle
+document.getElementById("rectangleBtn").addEventListener("click", function () {
+  let width = "width";
+  let length = "length";
+
+  let area = CalculateArea(width, length);
+
+  let displayValue = document.getElementById("displayValueRectangle");
+  if (typeof area === "number") {
+    displayValue.innerHTML = `
+      <p >Area (A) = ${area} cm<sup>2</sup></p>
+    `;
+  } else {
+    displayValue.innerHTML = `
+      <p id="displayErrorRectangle">Insert Valid Input</p>
+    `;
+
+    let displayError = document.getElementById("displayErrorRectangle");
+    displayError.style.color = "#f23737";
+  }
+});
+
+/* document.getElementById("triangleBtn").addEventListener("click", function () {
   let base = parseFloat(document.getElementById("base").value);
   let height = parseFloat(document.getElementById("height").value);
 
@@ -26,4 +80,4 @@ document.getElementById("rectangleBtn").addEventListener("click", function () {
   } else {
     displayValue.innerText = `${calculate}`;
   }
-});
+}); */
