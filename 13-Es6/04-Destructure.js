140d5f51d881c151546a1b23ded422869a4bc188
rubaiiyat <abirr1516@gmail.com>
2024-11-12 00:00:38 +0600

    Destructure

---
+++ 13-Es6/04-Destructure.js
@@ -0,0 +1,13 @@
+const person = {
+  firstName: "John",
+  lastName: "Kabir",
+  age: 25,
+  email: "John@gmail.com",
+};
+
+const { firstName, lastName, age, email } = person;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(email);
