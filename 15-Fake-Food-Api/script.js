267c565827c307833178571630fe5220431d88f4
rubaiiyat <abirr1516@gmail.com>
2024-11-18 23:20:50 +0600

    Displaying all data

--- 15-Fake-Food-Api/script.js
+++ 15-Fake-Food-Api/script.js
@@ -1,1 +1,40 @@
-console.log("Working");
+function ourApi() {
+  fetch("https://dummyjson.com/recipes")
+    .then((res) => res.json())
+    .then((data) => displayData(data.recipes));
+}
+
+ourApi();
+
+function displayData(data) {
+  const totalFood = document.getElementById("totalFood");
+  totalFood.innerText = data.length;
+  data.forEach((recipes) => {
+    const mainDiv = document.getElementById("mainDiv");
+
+    const newDiv = document.createElement("div");
+    newDiv.classList =
+      "card bg-base-100 w-96 shadow-xl hover:shadow-2xl hover:cursor-pointer";
+
+    newDiv.innerHTML = `
+
+    <figure class="px-10 pt-10">
+            <img
+              src="${recipes.image}"
+              alt="Shoes"
+              class="rounded-xl"
+            />
+          </figure>
+          <div class="card-body items-center text-center">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions">
+              <button class="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+    
+    `;
+
+    mainDiv.appendChild(newDiv);
+  });
+}
